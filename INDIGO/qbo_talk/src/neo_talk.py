9ca9b486993ab21254279940976f693d490f3181
elpimous <elpimous12@orange.fr>
2014-12-16 22:39:31 +0100

    some changes on neo_talk.py

--- INDIGO/qbo_talk/src/neo_talk.py
+++ INDIGO/qbo_talk/src/neo_talk.py
@@ -8,11 +8,20 @@
 import rospy
 from qbo_talk.srv import Text2Speach # read service content (words we want to be spoken)
 import os
+import subprocess
 
 # Some possibilities of different languages
 fr1_speak = "espeak -a 70 -s 140 -p50 -v mb/mb-fr1 \"%s\" | mbrola -e -C \"n n2\" /usr/share/mbrola/voices/fr1 - -.au | paplay"
 en1_speak = "espeak -a 70 -s 140 -p50 -v mb/mb-en1 \"%s\" | mbrola -e -C \"n n2\" /usr/share/mbrola/voices/en1 - -.au | paplay"
 
+
+def run_process(command = ""):
+    if command != "":
+        return os.system(command)
+    else:
+        return -1
+            
+            
 class talk():
 
     def __init__(self):
@@ -20,9 +29,11 @@ class talk():
         rospy.init_node('talk', anonymous=True)
         fr1 = rospy.Service('say_fr1', Text2Speach, self.fr1_talk)
         en1 = rospy.Service('say_en1', Text2Speach, self.en1_talk)
-
+        
+        
     def fr1_talk(self, speak):
-        os.system(fr1_speak % speak.sentence)
+       # os.system(fr1_speak % speak.sentence)
+        run_process(fr1_speak % speak.sentence)        
         return []
 
     def en1_talk(self, speak):
