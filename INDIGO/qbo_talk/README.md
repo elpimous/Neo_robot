36881375752233fc8c98189ef8c6597f2c76ed56
elpimous <elpimous12@orange.fr>
2015-05-22 22:51:19 +0200

    some changes, for XTION PRO MIC use, and automatic mute/unmute no need qbo_audio_control

--- INDIGO/qbo_talk/README.md
+++ INDIGO/qbo_talk/README.md
@@ -29,4 +29,29 @@ open one terminal : rosrun qbo_talk neo_talk.py
 in other terminal : rosservice call /say_fr1 "salut tout le monde"
                     rosservice call /say_en1 "hello world"
 
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+NEW :
+
+	A MODIFICATION FOR ASUS XTION MICS (QUITE BETTER THAN QBO ONES)
+        BE SURE TO HAVE PRIMESENSE MIC WORKING (TEST IT ON AUDACITY, you'll see an enormous difference !!!)
+	
+	no need qbo_audio_control.
+	faster ! nearly 1.5 seconds per process, and a clear recording
+
+        When you speak a sentence, xtion mic mute
+	when finished speaking, xtion mic unmute
+
+	***   neo_talk_to_xtion_mic.py   ***
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+SOON :
+
+	an alternative to ESPEAK / MBROLA.
+
+        SVOX PICO use in qbo_talk
+
+	quality between mbrola and acapela (french, dutch, english, spanish, and perhaps more...)
+
 
---
+++ INDIGO/qbo_talk/src/neo_talk_to_xtion_mic.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Vincent FOUCAULT
+# Here is a text-to-speech wrapper for Espeak
+# It's part of QBO robot ros softwares
+
+
+import rospy
+from qbo_talk.srv import Text2Speach # read service content (words we want to be spoken)
+import os
+import subprocess
+
+# Some possibilities of different languages
+fr1_speak = "espeak -a 70 -s 140 -p50 -v mb/mb-fr1 \"%s\" | mbrola -e -C \"n n2\" /usr/share/mbrola/voices/fr1 - -.au | paplay"
+#fr1_speak = "padsp swift \"%s\""
+en1_speak = "espeak -a 70 -s 140 -p50 -v mb/mb-en1 \"%s\" | mbrola -e -C \"n n2\" /usr/share/mbrola/voices/en1 - -.au | paplay"
+
+
+def run_process(command = ""):
+    if command != "":
+        return os.system(command)
+    else:
+        return -1
+            
+            
+class talk():
+
+    def __init__(self):
+
+        rospy.init_node('talk', anonymous=True)
+        fr1 = rospy.Service('say_fr1', Text2Speach, self.fr1_talk)
+        en1 = rospy.Service('say_en1', Text2Speach, self.en1_talk)
+        
+        
+    def fr1_talk(self, speak):
+        run_process("amixer -c 1 sset Mic,1 0%")
+        os.system(fr1_speak % speak.sentence) 
+        run_process("amixer -c 1 sset Mic,1 75%")
+        return []
+
+    def en1_talk(self, speak):
+        os.system(en1_speak % speak.sentence)
+        return []
+
+if __name__ == '__main__':
+    try:
+        talk = talk()
+        rospy.spin()
+    except rospy.ROSInterruptException: pass
+
+
