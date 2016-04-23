662d113309981a38c2ab3781d6d0377061c0e914
elpimous <elpimous12@orange.fr>
2016-04-23 23:38:52 +0200

    updates

--- INDIGO/qbo_play_music/src/play_music_new.py
+++ INDIGO/qbo_play_music/src/play_music_new.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 #********************************************************************
 #
-#
 #           A SIMPLE MEDIA PLAYER CONTROLLED BY VOICE
-#
 #                READ TEXT FILE FOR DEPENDENCIES
 #
 #             ANSWER SPOKEN WITH QBO_TALK (PICO2WAVE)
 #
+#           ** Pause music, just with gently head movement 
+#                     by humain interaction **
+#
 #                     elpimous12@orange.fr
 #
+#                  version 1.02 (23 avril 2016)
 #
 #********************************************************************
 
@@ -36,18 +37,19 @@ class mediaPlayer():
   def __init__(self):
 
         rospy.init_node('qbo_play_music')
+        rospy.on_shutdown(self.exit)
         self.client_speak = rospy.ServiceProxy("/say_fr1", Text2Speach)
         self.en_attente_ordres = False
         rospy.Subscriber('listened', String, self.Choice)
-        rospy.Subscriber('hand_detector', String, self.hand_detector)
-	self.pkg_dir = roslib.packages.get_pkg_dir("qbo_play_music")
+        rospy.Subscriber('/head_joy', String, self.pause_detector)
+        self.pkg_dir = roslib.packages.get_pkg_dir("qbo_play_music")
         self.dir = self.pkg_dir+"/audios/"
         self.count = len([f for f in os.listdir(self.dir) if os.path.isfile(os.path.join(self.dir, f))])
         self.song_info = ""
         self.started = False
         self.Detected = False
         self.waitingForOrders = True
-        self.iSeeYourHand = ""
+        self.ok_I_pause = ""
         self.sentence = ""
         self.secs = int(0)
         self.pause = False
@@ -60,6 +62,7 @@ class mediaPlayer():
 	self.client_speak(str(text))
 
   def get_song_info_2(self): # return name of actual music
+   try:
     (ph_out, ph_err, ph_ret) = runCmd("nyxmms2 list")
     out_str = str(ph_out)
     self.song_info = out_str.split("->")
@@ -73,15 +76,19 @@ class mediaPlayer():
     self.song_info = self.song_info.strip()
     self.song_info = self.song_info.split(" - ")
     return self.song_info
+   except :
+    return "titre inconnu"
 
-  def hand_detector(self, msg):
-      self.iSeeYourHand = msg.data
+  def pause_detector(self, msg):
+      self.ok_I_pause = msg.data
 
   def Cut_mics(self):
-          runCmd("amixer -c 1 sset Mic,1 nocap")
           runCmd("amixer -c 1 sset Mic,0 nocap")
-          sleep(0.2)
+          sleep(0.1)
 
+  def Open_mics(self):
+          sleep(0.1)
+          runCmd("amixer -c 1 sset Mic,0 cap")
 
   def Choice(self, msg):
       if self.waitingForOrders == True :# and not self.started :
@@ -93,6 +100,8 @@ class mediaPlayer():
           self.speak_this(choice(howManyTiltes))
           self.pause = False
 
+        #TODO travailler sur les titres des zic (23 avril 2016)
+        """
         # Title name
 	if self.sentence.find("nom") > -1 or self.sentence.find("titre") > -1 :
           print " - TITRE DE MUSIQUE DEMANDE,"
@@ -100,6 +109,7 @@ class mediaPlayer():
           self.speak_this("Titre en cours, "+self.song_info[0].replace("_"," "))
           self.sentence = "blabla"
           self.pause = False
+        """
 
         # Previous
 	if self.sentence.find("précédent") > -1 or self.sentence.find("précédente") > -1 :
@@ -118,11 +128,11 @@ class mediaPlayer():
           self.pause = False
 
         # stop
-	if self.sentence.find("stop") > -1 :
+	if self.sentence.find("stop la musique") > -1 :
           print " - ARRET DEMANDE."
           runCmd("xmms2 stop") # play server
           self.sentence = "blabla"
-          sleep(1)
+          sleep(0.1)
           self.speak_this("D'accord, A +,")
           run_process("rosnode kill /qbo_play_music")
           run_process("rosnode kill /qbo_listen")
@@ -131,31 +141,32 @@ class mediaPlayer():
 	if self.sentence.find("lecture") > -1 :
           print " - LECTURE DEMANDEE,"
           self.get_song_info_2()
-          self.speak_this("Bien, je lis la musique, "+self.song_info[0].replace("_"," "))
+          #self.speak_this("Bien, je lis la musique, ")#+self.song_info[0].replace("_"," "))
           self.pause = False
-          #self.speak_this("Je démarre la lecture,")
+          self.speak_this("Je démarre la lecture,")
+          self.Cut_mics()
           runCmd("xmms2 play") # play server
           self.started = True
       else : pass 
 
 
   def main(self):
-    run_process("amixer -c 1 sset Mic,1 0")
     self.speak_this("Les musiques sont chargées. Prononce le mot - Lecture, - quand tu es prêt,")
 
     while not rospy.is_shutdown(): # loop each second
       # PAUSE WITH HAND
       #self.waitingForOrders = False
-      if self.iSeeYourHand == "seen":
+      rospy.wait_for_message('/head_joy', String, timeout=None)
+      if self.ok_I_pause == "left" or self.ok_I_pause == "right":
           self.waitingForOrders = True
-          print "hand detected !"
-
+          self.Open_mics()
+          print "pause needed"
           #self.started = False
           self.sentence = "blabla"
           if self.pause == False :
             runCmd("xmms2 pause") # play server
             self.speak_this("Oui?")
-            sleep(0.5)
+            sleep(0.1)
             print " En attente d'ordres..."
             self.pause = True
 
@@ -170,6 +181,13 @@ class mediaPlayer():
           runCmd("xmms2 play") # play server
           self.started = True
 
+  def exit(self):
+	runCmd("xmms2 clear")
+        sleep(0.1)
+	runCmd("xmms2 exit")
+        sleep(0.1)
+        run_process("rosnode kill /qbo_head_joy")
+    
 
 if __name__ == '__main__':
     try:
